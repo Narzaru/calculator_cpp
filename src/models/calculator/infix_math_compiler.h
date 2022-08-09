30404e8be41b0138ad73396e00fa949be1413977
Narzaru <yaroslahek@gmail.com>
2022-08-09 22:31:35 +0700

    edited main include file

--- src/models/calculator/infix_math_compiler.h
+++ src/models/calculator/infix_math_compiler.h
@@ -1,10 +1,6 @@
 #ifndef SRC_MODELS_CALCULATOR_INFIX_MATH_COMPILER_H_
 #define SRC_MODELS_CALCULATOR_INFIX_MATH_COMPILER_H_
 
-#include "lexeme_analyzer.h"
-#include "lexical_analyzer.h"
-#include "reverse_polish_notation_former.h"
-#include "reverse_polish_notation_calc.h"
-#include "token.h"
+#include "calculator.h"
 
 #endif  // SRC_MODELS_CALCULATOR_INFIX_MATH_COMPILER_H_
