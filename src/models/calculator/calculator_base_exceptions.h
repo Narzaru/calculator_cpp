ed848a77fce1cda875e9e39d118736f2fc5b4419
Narzaru <yaroslahek@gmail.com>
2022-08-10 22:23:14 +0700

    Implemented an interface for my exception base classes

---
+++ src/models/calculator/calculator_base_exceptions.h
@@ -0,0 +1,12 @@
+#ifndef SRC_MODELS_CALCULATOR_CALCULATOR_BASE_EXCEPTIONS_H_
+#define SRC_MODELS_CALCULATOR_CALCULATOR_BASE_EXCEPTIONS_H_
+
+#include <exception>
+namespace s21::exception {
+class IMyBaseException : public std::exception {
+ public:
+  [[nodiscard]] virtual const char *who() const noexcept = 0;
+};
+}  // namespace s21
+
+#endif  // SRC_MODELS_CALCULATOR_CALCULATOR_BASE_EXCEPTIONS_H_
