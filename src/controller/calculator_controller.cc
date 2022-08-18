eb906c3bfa772942e7700e8b04c3f0fd81ca2d27
Narzaru <yaroslahek@gmail.com>
2022-08-19 00:20:18 +0700

    Fixed typo Improved some functionality

--- src/controller/calculator_controller.cc
+++ src/controller/calculator_controller.cc
@@ -1,13 +1,30 @@
 #include "calculator_controller.h"
 
 namespace s21 {
-CalcultatorConstroller::CalcultatorConstroller(calculator::Calculator *calc)
+CalculatorController::CalculatorController(calculator::Calculator *calc)
     : calculator_(calc) {}
 
-CalcultatorConstroller::~CalcultatorConstroller() {}
+CalculatorController::~CalculatorController() = default;
 
-double CalcultatorConstroller::evaluate(std::string string) {
+double CalculatorController::evaluate(const std::string &string) {
   return calculator_->calculate(string);
 }
+s21::UniformlyDiscreteFunction CalculatorController::GetFunction(const std::string &expression,
+                                                                 const std::string &sx_min,
+                                                                 const std::string &sx_max,
+                                                                 const std::string &sy_min,
+                                                                 const std::string &sy_max,
+                                                                 int dots_count) {
+  double x_min = std::stod(sx_min);
+  double x_max = std::stod(sx_max);
+  double y_min = std::stod(sy_min);
+  double y_max = std::stod(sy_max);
+  s21::UniformlyDiscreteFunction ret_function(dots_count, x_min, x_max);
+  ret_function.CalculateYValues(expression, calculator_);
+  ret_function.SetDomain(y_min, y_max);
+  return ret_function;
+}
+
+
 
 }  // namespace s21
--- src/controller/calculator_controller.h
+++ src/controller/calculator_controller.h
@@ -2,16 +2,23 @@
 #define SRC_CONTROLLER_CALCULATOR_CONTROLLER_H_
 
 #include <string>
+#include "function.h"
 
 #include "../models/calculator/calculator.h"
 
 namespace s21 {
-class CalcultatorConstroller {
+class CalculatorController {
  public:
-  explicit CalcultatorConstroller(calculator::Calculator *calc);
-  ~CalcultatorConstroller();
+  explicit CalculatorController(calculator::Calculator *calc);
+  ~CalculatorController();
 
-  double evaluate(std::string string);
+  double evaluate(const std::string &string);
+  s21::UniformlyDiscreteFunction GetFunction(const std::string &expression,
+                                             const std::string &x_min,
+                                             const std::string &x_max,
+                                             const std::string &y_min,
+                                             const std::string &y_max,
+                                             int dots_count);
 
  private:
   calculator::Calculator *calculator_;
